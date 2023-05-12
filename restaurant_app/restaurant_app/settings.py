4ee98a650d6fe233912d364406b805ed37fe4574
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2023-05-11 23:07:15 -0300

    Making the menu page structure

--- restaurant_app/restaurant_app/settings.py
+++ restaurant_app/restaurant_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-faw4qv4$bvl@7hs3719jl_fbx^zu7xgkv1wzie)wcfb!ir(haz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '10.0.2.2', '127.0.0.1']
 
 
 # Application definition
@@ -114,6 +114,8 @@ USE_I18N = True
 USE_TZ = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
